044b0d739f39ff17843936deb24a3191db3a077d
JosePluymaekers <79280564+JosePluymaekers@users.noreply.github.com>
2021-03-11 21:48:43 +0100

    Add files via upload

---
+++ JavascriptOpdracht3/script.js
@@ -0,0 +1,30 @@
+// this is my 3rd javascript code!
+
+console.log("mag jij wel naar binnen?")
+
+//de if statement
+
+const leeftijdcheck = 52
+
+if (leeftijdcheck > 17) {
+    console.log("Je mag naar binnen. Welkom in onze kroeg")
+} else {
+    console.log("helaas, jij moet buiten blijven")
+}
+
+const isFemale = true
+
+if (isFemale) {
+    console.log("jij bent een vrouw, welkom op ons feest")
+} else {
+    console.log("Weg jij vieze vent die je bent. Het is ladies only")
+}
+
+
+const driverStatus = "zuiperd"
+
+if (driverStatus == "bob") {
+    console.log("heel verstandig")
+} else {
+    console.log("we bellen graag een taxi voor je")
+}
